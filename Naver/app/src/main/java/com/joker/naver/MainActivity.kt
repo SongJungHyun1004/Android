ae8f5f97252fb0e8aaf22a2f3c31887cc20a1072
SongJungHyun <201802112@o.cnu.ac.kr>
2024-03-30 23:16:11 +0900

    feat: search bar

--- Naver/app/src/main/java/com/joker/naver/MainActivity.kt
+++ Naver/app/src/main/java/com/joker/naver/MainActivity.kt
@@ -3,7 +3,11 @@ package com.joker.naver
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,17 +22,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.Notifications
-import androidx.compose.material.icons.filled.Paid
-import androidx.compose.material.icons.filled.Sms
-import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Paid
 import androidx.compose.material.icons.outlined.Sms
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -36,9 +38,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.joker.naver.ui.theme.GreenDot_Gradient
+import com.joker.naver.ui.theme.GreenDot_Green
 import com.joker.naver.ui.theme.NaverTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,11 +55,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             NaverTheme {
                 // A surface container using the 'background' color from the theme
+                val colorMode = MaterialTheme.colorScheme.surface
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomeScreen()
+                    HomeScreen(colorMode)
                 }
             }
         }
@@ -59,7 +68,9 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(
+    colorMode: Color
+) {
     LazyColumn(
         modifier = Modifier
             .fillMaxWidth(),
@@ -69,7 +80,7 @@ fun HomeScreen() {
             Column {
                 TopBar()
                 Spacer(modifier = Modifier.height(70.dp))
-                SearchBar()
+                SearchBar(colorMode)
                 Spacer(modifier = Modifier.height(20.dp))
                 Menu()
                 Spacer(modifier = Modifier.height(70.dp))
@@ -92,38 +103,129 @@ fun TopBar() {
             .fillMaxWidth(),
         contentAlignment = Alignment.CenterStart,
     ) {
-        Row (
+        Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
         ) {
             val iconSize = 40.dp
-            Row (
+            Row(
                 modifier = Modifier.padding(start = 5.dp)
             ) {
-                Icon(imageVector = Icons.Outlined.Menu, contentDescription = "menu", modifier = Modifier.size(iconSize).padding(horizontal = 5.dp))
-                Icon(imageVector = Icons.Outlined.Paid, contentDescription = "pay", modifier = Modifier.size(iconSize).padding(horizontal = 5.dp))
+                Icon(
+                    imageVector = Icons.Outlined.Menu,
+                    contentDescription = "menu",
+                    modifier = Modifier
+                        .size(iconSize)
+                        .padding(horizontal = 5.dp)
+                )
+                Icon(
+                    imageVector = Icons.Outlined.Paid,
+                    contentDescription = "pay",
+                    modifier = Modifier
+                        .size(iconSize)
+                        .padding(horizontal = 5.dp)
+                )
             }
-            Row (
+            Row(
                 modifier = Modifier.padding(end = 5.dp)
             ) {
-                Icon(imageVector = Icons.Outlined.Sms, contentDescription = "talk", modifier = Modifier.size(iconSize).padding(horizontal = 5.dp))
-                Icon(imageVector = Icons.Outlined.Notifications, contentDescription = "notify", modifier = Modifier.size(iconSize).padding(horizontal = 5.dp))
-                Icon(imageVector = Icons.Default.AccountCircle, contentDescription = "account", modifier = Modifier.size(iconSize).padding(horizontal = 5.dp))
+                Icon(
+                    imageVector = Icons.Outlined.Sms,
+                    contentDescription = "talk",
+                    modifier = Modifier
+                        .size(iconSize)
+                        .padding(horizontal = 5.dp)
+                )
+                Icon(
+                    imageVector = Icons.Outlined.Notifications,
+                    contentDescription = "notify",
+                    modifier = Modifier
+                        .size(iconSize)
+                        .padding(horizontal = 5.dp)
+                )
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "account",
+                    modifier = Modifier
+                        .size(iconSize)
+                        .padding(horizontal = 5.dp)
+                )
             }
         }
     }
 }
 
 @Composable
-fun SearchBar() {
+fun SearchBar(
+    colorMode: Color
+) {
     Box(
         modifier = Modifier
             .height(60.dp)
-            .fillMaxWidth()
-            .background(color = Color.Green),
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center,
     ) {
-        Row {
-            Text(text = "Search Bar")
+        Box(
+            modifier = Modifier
+                .height(55.dp)
+                .width(380.dp)
+                .border(
+                    BorderStroke(1.dp, SolidColor(GreenDot_Green)),
+                    shape = RoundedCornerShape(30.dp),
+                )
+                .background(Color.Transparent, RoundedCornerShape(30.dp)),
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+            ) {
+                Row {
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Image(
+                        painter = painterResource(id = R.drawable.naver_logo),
+                        contentDescription = "naver logo",
+                        modifier = Modifier
+                            .size(30.dp)
+                    )
+                }
+                Button(
+                    modifier = Modifier
+                        .width(260.dp),
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent,
+                        contentColor = Color.Gray.copy(alpha = 0.7f)
+                    ),
+                    contentPadding = PaddingValues(start = 0.dp)
+                ) {
+                    Text(
+                        text = "검색어를 입력해주세요.",
+                        fontSize = 18.sp,
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Start,
+                    )
+                }
+//                Button(onClick = { /* 버튼 클릭 시 수행할 작업 */ },
+//                    modifier = Modifier.width(200.dp).background(color = Color.Cyan), // 버튼을 최대 너비로 설정
+//                    contentPadding = PaddingValues(end = 16.dp) // 버튼의 오른쪽 패딩 조절
+//                ) {
+//                    Text("버튼",
+//                        modifier = Modifier.fillMaxWidth(), // 텍스트를 최대 너비로 설정
+//                        textAlign = TextAlign.Start // 텍스트를 오른쪽 정렬
+//                    )
+//                }
+                val brush = Brush.linearGradient(GreenDot_Gradient)
+                Canvas(
+                    modifier = Modifier
+                        .size(30.dp),
+                    onDraw = {
+                        drawCircle(brush)
+                        drawCircle(color = colorMode, radius = 18f)
+                    }
+                )
+            }
         }
     }
 }
@@ -164,17 +266,19 @@ fun Weather() {
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceEvenly,
         ) {
-            Box(modifier = Modifier
-                .width(180.dp)
-                .fillMaxHeight()
-                .background(color = Color.LightGray)
+            Box(
+                modifier = Modifier
+                    .width(180.dp)
+                    .fillMaxHeight()
+                    .background(color = Color.LightGray)
             ) {
                 Text(text = "Temperature")
             }
-            Box(modifier = Modifier
-                .width(180.dp)
-                .fillMaxHeight()
-                .background(color = Color.LightGray)
+            Box(
+                modifier = Modifier
+                    .width(180.dp)
+                    .fillMaxHeight()
+                    .background(color = Color.LightGray)
             ) {
                 Text(text = "Dust")
             }
--- Naver/app/src/main/java/com/joker/naver/ui/theme/Color.kt
+++ Naver/app/src/main/java/com/joker/naver/ui/theme/Color.kt
@@ -5,9 +5,16 @@ import androidx.compose.ui.graphics.Color
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val DarkSurface = Color(0xFF303033)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val LightSurface = Color.White
 
-val color = Color(0xFFA6E291)
+val color = Color(0xFF303033)
+
+val GreenDot_Green = Color(0xFF03EB64)
+val GreenDot_Mint = Color(0xFF14E6BE)
+val GreenDot_Blue = Color(0xFF1EC8E8)
+val GreenDot_Gradient = listOf(GreenDot_Green, GreenDot_Green, GreenDot_Mint, GreenDot_Blue)
