44cad44a182f99b08d606bda1bf2c78bceadec86
SongJungHyun <201802112@o.cnu.ac.kr>
2024-04-06 23:40:12 +0900

    feat: menu

--- Naver/app/src/main/java/com/joker/naver/MainActivity.kt
+++ Naver/app/src/main/java/com/joker/naver/MainActivity.kt
@@ -38,10 +38,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -49,6 +51,7 @@ import androidx.compose.ui.unit.sp
 import com.joker.naver.ui.theme.GreenDot_Gradient
 import com.joker.naver.ui.theme.GreenDot_Green
 import com.joker.naver.ui.theme.NaverTheme
+import com.joker.naver.ui.theme.color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -83,7 +86,7 @@ fun HomeScreen(
                 Spacer(modifier = Modifier.height(65.dp))
                 SearchBar(colorMode)
                 Spacer(modifier = Modifier.height(20.dp))
-                Menu()
+                Menu(colorMode)
                 Spacer(modifier = Modifier.height(65.dp))
                 BannerAds()
                 Spacer(modifier = Modifier.height(7.dp))
@@ -224,14 +227,150 @@ fun SearchBar(
 }
 
 @Composable
-fun Menu() {
+fun MenuIcon(
+    colorMode: Color,
+    img: @Composable () -> Unit,
+    txt: String,
+) {
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Box(
+            modifier = Modifier
+                .size(45.dp)
+                .shadow(elevation = 1.dp, RoundedCornerShape(15.dp))
+                .clip(RoundedCornerShape(15.dp))
+                .background(colorMode),
+            contentAlignment = Alignment.Center,
+        ) {
+            img()
+        }
+        Spacer(modifier = Modifier.height(5.dp))
+        Text(text = txt, fontSize = 11.sp)
+    }
+}
+
+@Composable
+fun Menu(
+    colorMode: Color
+) {
     Box(
         modifier = Modifier
             .height(150.dp)
-            .fillMaxWidth()
-            .background(color = Color.Green),
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center,
     ) {
-        Text(text = "Menu")
+        Box(
+            modifier = Modifier
+                .width(280.dp)
+                .fillMaxHeight()
+                .background(color = colorMode) // colorMode
+                .padding(start = 8.dp, top = 2.dp, bottom = 2.dp)
+        ) {
+            Column (
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    MenuIcon(
+                        colorMode = colorMode,
+                        img = {
+                            Image(
+                                painter = painterResource(id = R.drawable.news_pan),
+                                contentDescription = "news_pan icon",
+                                modifier = Modifier.size(30.dp),
+                            )
+                        },
+                        txt = "뉴스판",
+                    )
+                    MenuIcon(
+                        colorMode = colorMode,
+                        img = {
+                            Image(
+                                painter = painterResource(id = R.drawable.showping_pan),
+                                contentDescription = "showping_pan icon",
+                                modifier = Modifier.size(30.dp),
+                            )
+                        },
+                        txt = "쇼핑판",
+                    )
+                    MenuIcon(
+                        colorMode = colorMode,
+                        img = {
+                            Image(
+                                painter = painterResource(id = R.drawable.economy_pan),
+                                contentDescription = "economy_pan icon",
+                                modifier = Modifier.size(30.dp),
+                            )
+                        },
+                        txt = "경제판",
+                    )
+                    MenuIcon(
+                        colorMode = colorMode,
+                        img = {
+                            Image(
+                                painter = painterResource(id = R.drawable.clip_pan),
+                                contentDescription = "clip_pan icon",
+                                modifier = Modifier.size(30.dp),
+                            )
+                        },
+                        txt = "클립판",
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    MenuIcon(
+                        colorMode = colorMode,
+                        img = {
+                            Image(
+                                painter = painterResource(id = R.drawable.mail),
+                                contentDescription = "mail icon",
+                                modifier = Modifier.size(30.dp),
+                            )
+                        },
+                        txt = "메일",
+                    )
+                    MenuIcon(
+                        colorMode = colorMode,
+                        img = {
+                            Image(
+                                painter = painterResource(id = R.drawable.cafe),
+                                contentDescription = "cafe icon",
+                                modifier = Modifier.size(30.dp),
+                            )
+                        },
+                        txt = "카페",
+                    )
+                    MenuIcon(
+                        colorMode = colorMode,
+                        img = {
+                            Image(
+                                painter = painterResource(id = R.drawable.blog),
+                                contentDescription = "blog icon",
+                                modifier = Modifier.size(30.dp),
+                            )
+                        },
+                        txt = "블로그",
+                    )
+                    MenuIcon(
+                        colorMode = colorMode,
+                        img = {
+                            Image(
+                                painter = painterResource(id = R.drawable.more),
+                                contentDescription = "more icon",
+                                modifier = Modifier.size(30.dp),
+                            )
+                        },
+                        txt = "",
+                    )
+                }
+            }
+        }
     }
 }
 
--- Naver/app/src/main/java/com/joker/naver/ui/theme/Color.kt
+++ Naver/app/src/main/java/com/joker/naver/ui/theme/Color.kt
@@ -12,7 +12,7 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 val LightSurface = Color.White
 
-val color = Color(0xFF303033)
+val color = Color(0xFFF3F3F3)
 
 val GreenDot_Green = Color(0xFF03EB64)
 val GreenDot_Mint = Color(0xFF14E6BE)
